c25a2aef016c3f3f6542e418faa2cacf564598e6
Steffen Kruse Hansen <sha@dynamicweb.dk>
2023-01-14 17:01:46 +0100

    Initial commit

---
+++ src/OperationStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Dynamicweb.Ecommerce.CheckoutHandlers.QuickPayPaymentWindow
+{
+    internal class OperationStatus
+    {
+        public bool IsPending { get; set; }
+        public bool Succeded { get; set; }
+        public string StatusCode { get; set; }
+    }
+}
